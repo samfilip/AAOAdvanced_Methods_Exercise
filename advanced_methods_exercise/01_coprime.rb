3ee095293af3e2961ddc68c6382f049c95c33c83
Sam Filip <samjfilip@gmail.com>
2020-09-01 15:53:49 -0700

    Optimized code

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,15 +3,10 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 def coprime?(num_1,num_2)
-  nums = [num_1, num_2]
-  high = nums.max
-    (2..high).to_a.each do |n|
-        if num_1 % n === 0 && num_2 % n === 0
-            return false
-        end
-    end
-  return true
+  low = [num_1, num_2].min
+  (2..low).none? { |n| num_1 % n == 0 && num_2 % n == 0 }
 end
+
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
