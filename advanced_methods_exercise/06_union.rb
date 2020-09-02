c245935f00fbae48bf9ef33cafbd40e3f3a93c09
Sam Filip <samjfilip@gmail.com>
2020-09-01 17:23:36 -0700

    added alternate solution

--- advanced_methods_exercise/06_union.rb
+++ advanced_methods_exercise/06_union.rb
@@ -4,5 +4,9 @@ def union(*arrays)
     arrays.flatten
 end
 
+# def union(*arrays)
+#     arrays.inject { |all_elements, arr| all_elements + arr }
+# end
+
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
